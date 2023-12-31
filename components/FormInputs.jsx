d0d1e2a095054d7cedcf421932691703cebf9d3f
Ricardo Parra <ricardo.prxr16@gmail.com>
2023-12-30 23:18:28 -0400

    LogIn con validaciones de react-hook-form

--- components/FormInputs.jsx
+++ components/FormInputs.jsx
@@ -1,50 +1,74 @@
 import React, { useState } from 'react';
 import { TextInput, Button, Text } from 'react-native-paper';
-import { View } from 'react-native';
+import { View } from 'react-native'; 
+import { Controller } from 'react-hook-form';
 
-export const CustomInput = ({ placeholder, default_value = '', icon, style, ...props }) => {
-  const [text, setText] = useState(default_value);
-
-  return (
-    <TextInput
-      style={style}
-      label={placeholder}
-      mode="outlined"
-      value={text}
-      onChangeText={text => setText(text)}
-      {...props}
-    />
-  );
+export const CustomInput = ({ rules, control, name, placeholder, default_value = '', style, ...props }) => {
+	const [text, setText] = useState(default_value);
+	return (
+		<Controller 
+			control={control}
+			name={name}
+			rules={rules}
+			render={({field : { value, onBlur, onChange}, fieldState : { error } }) => (
+					<>
+						<TextInput
+							{...props}
+							style={style}
+							label={placeholder}
+							mode="outlined"
+							value={value}
+							error={error}
+							onBlur={onBlur}
+							onChangeText={onChange}
+						/>
+						{error && ( <Text className="text-theme-error font-bold">{error.message}</Text> )}
+					</>
+				)}
+		/>
+	);
 };
 
-export const PasswordInput = ({ placeholder, default_value = '', style, ...props }) => {
-  const [text, setText] = useState(default_value);
-  const [passwordTypeText, setpasswordTypeText] = useState(true);
+export const PasswordInput = ({ rules, control, name, placeholder, default_value = '', style, ...props }) => {
+	const [text, setText] = useState(default_value);
+	const [passwordTypeText, setpasswordTypeText] = useState(true);
 
-  return (
-    <TextInput
-      style={style}
-      label={placeholder}
-      secureTextEntry={passwordTypeText}
-      mode="outlined"
-      value={text}
-      autoCapitalize="none"
-      onChangeText={text => setText(text)}
-      right={
-        <TextInput.Icon
-          icon={passwordTypeText ? 'eye' : 'eye-off'}
-          onPress={() => setpasswordTypeText(!passwordTypeText)} 
-        />}
-      {...props}
-    />
-  );
+	return (
+		<Controller 
+			control={control}
+			name={name}
+			rules={rules}
+			render={({field : { value, onBlur, onChange}, fieldState : { error } }) => (
+					<>
+						<TextInput
+							style={style}
+							label={placeholder}
+							secureTextEntry={passwordTypeText}
+							mode="outlined"
+							autoCapitalize="none"
+							value={value}
+							error={error}
+							onBlur={onBlur}
+							onChangeText={onChange}
+							right={
+								<TextInput.Icon
+									icon={passwordTypeText ? 'eye' : 'eye-off'}
+									onPress={() => setpasswordTypeText(!passwordTypeText)} 
+								/>}
+							{...props}
+						/>
+						{error && ( <Text className="text-theme-error font-bold">{error.message}</Text> )}
+					</>
+				)}
+		/>
+	);
 };
 
 export const AnchorText = ({children, additionalText = '', href, style}) => {
-  return (
-      <Button mode="text" onPress={href} style={style}>
-        <Text className="">{additionalText}</Text>
-        {children}
-      </Button>
-  );
+	return (
+			<Button mode="text" onPress={href} style={style}>
+				<Text className="">{additionalText}</Text>
+				{children}
+			</Button>
+	);
 }
--- screens/LogInScreen.jsx
+++ screens/LogInScreen.jsx
@@ -1,11 +1,18 @@
 import { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { View } from 'react-native';
-import { Text } from 'react-native-paper'
+import { Text, Button } from 'react-native-paper'
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { CustomInput, PasswordInput, AnchorText } from '../components/FormInputs';
+import { useForm } from "react-hook-form";
 
 export default function LogInScreen({ navigation }) {
+
+  const { control, handleSubmit, formState: { errors } } = useForm();
+  const onSubmit = (data) => {
+    console.log(data);
+  }
+
   return (
     <SafeAreaView className="flex-1 items-center justify-center bg-theme-background">
       <StatusBar style="auto"/>
@@ -18,16 +25,31 @@ export default function LogInScreen({ navigation }) {
         >
           Iniciar sesión
         </Text>
+
         <CustomInput
           placeholder="Email"
           autoComplete="email"
           autoCapitalize="none"
+          control={control}
+          name="email"
+          rules={{required : 'Complete el correo.', pattern: { value: /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g, message:'El correo tiene que ser válido'}}}
         />
         <PasswordInput 
           className="mt-[10px]"
+          control={control}
+          name="password"
           placeholder="Contraseña"
           autoComplete="password"
+          rules={{
+            required : 'Complete la contraseña.',
+            minLength: { value: 5, message: 'La contraseña debe tener mínimo 5 carácteres.'},
+            maxLength: { value: 20, message: 'La contraseña no puede pasar de los 20 carácteres.'}
+          }}
         />
+
+        <Button className="w-[50%] m-auto mt-4" mode="contained" onPress={handleSubmit(onSubmit)}>
+          Iniciar sesión
+        </Button>
       </View>
       <AnchorText
         className="" 
