105ddfd325b5da5d0648975c1442459c38c0efeb
Ricardo Parra <ricardo.prxr16@gmail.com>
2024-05-29 12:33:49 -0400

    fix: dependences updated

--- App.js
+++ App.js
@@ -1,14 +1,19 @@
-import { AppRegistry } from 'react-native';
-import { PaperProvider } from 'react-native-paper';
-import { name as appName } from './app.json';
-import { theme } from './global/theme';
-import Navigator from './navigate/Navigator.jsx'
+import { AppRegistry } from "react-native";
+import { PaperProvider } from "react-native-paper";
+import { name as appName } from "./app.json";
+import { theme } from "./global/theme";
+import Navigator from "./navigate/Navigator.jsx";
+import { NativeWindStyleSheet } from "nativewind";
+
+NativeWindStyleSheet.setOutput({
+  default: "native",
+});
 
 export default function App() {
-  console.log(theme.colors.primary)
+  console.log(theme.colors.primary);
   return (
     <PaperProvider theme={theme}>
-      <Navigator/>
+      <Navigator />
     </PaperProvider>
   );
 }
