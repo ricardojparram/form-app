7f67290038529f08c153df8fbb6f0c9f13ad2793
Ricardo Parra <ricardo.pxr16@gmail.com>
2024-06-05 19:11:19 -0400

    fix: selectScreen deleted

--- navigate/Navigator.jsx
+++ navigate/Navigator.jsx
@@ -7,7 +7,6 @@ import ProfileScreen from "../screens/ProfileScreen";
 import ChangePasswordScreen from "../screens/ChangePasswordScreen";
 import HomeScreen from "../screens/HomeScreen";
 import DashboardScreen from "../screens/DashboardScreen";
-import Select from "../screens/SelectScreen";
 
 const Stack = createNativeStackNavigator();
 export default function Navigator() {
@@ -26,7 +25,6 @@ export default function Navigator() {
         <Stack.Screen name="Recovery" component={PassRecoveryScreen} />
         <Stack.Screen name="Profile" component={ProfileScreen} />
         <Stack.Screen name="ChangePassword" component={ChangePasswordScreen} />
-        <Stack.Screen name="Select" component={Select} />
       </Stack.Navigator>
     </NavigationContainer>
   );
