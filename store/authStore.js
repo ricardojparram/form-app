a3ef51021a8cca2e3ef044c9fbf9254a4cc4d724
Ricardo Parra <ricardo.pxr16@gmail.com>
2024-06-05 19:08:08 -0400

    feat: authStore with isAuthenticated and checkSession

--- store/authStore.js
+++ store/authStore.js
@@ -8,11 +8,15 @@ import { urlEncode } from "../utils/urlEncode";
 export const useAuthStore = create(
   persist(
     (set, get) => ({
-      token: null,
+      token: "123123",
       user: null,
+      isAuthenticated: false,
+
       // setters
+      setIsAuthenticated: (isAuthenticated) => set({ isAuthenticated }),
       setToken: (token) => set({ token }),
       setUser: (user) => set({ user }),
+
       //methods
 
       login: async (sede, cedula, password) => {
@@ -29,11 +33,44 @@ export const useAuthStore = create(
           }),
         });
         const res = await request.json();
+        if (!res.token) {
+          return res;
+        }
         const { data } = jwtDecode(res.token);
         set({
           token: res.token,
           user: data,
         });
+        return true;
+      },
+
+      checkSession: async () => {
+        const token = get().token;
+        if (!token) {
+          set({ token: null, user: null, isAuthenticated: false });
+          return false;
+        }
+
+        try {
+          const request = await fetch(API_SRC + "?url=inventario", {
+            headers: {
+              Authorization: `Bearer ${token}`,
+            },
+          });
+
+          const res = await request.json();
+          console.log(res);
+          if (res.resultado === "error") {
+            set({ token: null, user: null });
+            return false;
+          } else {
+            return true;
+          }
+        } catch (error) {
+          console.error("Error checking session:", error);
+          set({ token: null, user: null });
+          return false;
+        }
       },
 
       logout: () =>
