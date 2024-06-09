ff89dfbc5503472cd5f3265da82f3278cc9e835b
Ricardo Parra <ricardo.pxr16@gmail.com>
2024-06-08 22:35:25 -0400

    feat: new nested navigator

--- App.js
+++ App.js
@@ -2,12 +2,12 @@ import { AppRegistry } from "react-native";
 import { PaperProvider } from "react-native-paper";
 import { name as appName } from "./app.json";
 import { theme } from "./global/theme";
-import Navigator from "./navigate/Navigator.jsx";
-import { useAuthStore } from "./store/authStore.js";
+import { LoginNav } from "./navigate/Navigator";
+
 export default function App() {
   return (
     <PaperProvider theme={theme}>
-      <Navigator />
+      <LoginNav />
     </PaperProvider>
   );
 }
--- navigate/Navigator.jsx
+++ navigate/Navigator.jsx
@@ -1,31 +1,45 @@
-import { NavigationContainer } from "@react-navigation/native";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import LogInScreen from "../screens/LogInScreen";
-import SignInScreen from "../screens/SignInScreen";
-import PassRecoveryScreen from "../screens/PassRecoveryScreen";
-import ProfileScreen from "../screens/ProfileScreen";
-import ChangePasswordScreen from "../screens/ChangePasswordScreen";
 import HomeScreen from "../screens/HomeScreen";
-import DashboardScreen from "../screens/DashboardScreen";
+import LogInScreen from "../screens/LogInScreen";
+import PassRecoveryScreen from "../screens/PassRecoveryScreen";
+import ChangePasswordScreen from "../screens/ChangePasswordScreen";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { createDrawerNavigator } from "@react-navigation/drawer";
+import { NavigationContainer } from "@react-navigation/native";
+import { useAuthStore } from "../store/authStore";
 
 const Stack = createNativeStackNavigator();
-export default function Navigator() {
+const Drawer = createDrawerNavigator();
+
+export const LoggedInNav = () => {
+  const isAuthenticated = useAuthStore((state) => state.isAuthenticated);
+  console.log("isAuthenticated: " + isAuthenticated);
+  return (
+    <Drawer.Navigator
+      initialRouteName="Home"
+      screenOptions={{
+        headerShown: { isAuthenticated },
+      }}
+    >
+      <Drawer.Screen name="Home" component={HomeScreen} />
+      <Drawer.Screen name="LogIn" component={LogInScreen} />
+    </Drawer.Navigator>
+  );
+};
+
+export const LoginNav = () => {
   return (
     <NavigationContainer>
       <Stack.Navigator
-        initialRouteName="Dashboard"
+        initialRouteName="LogIn"
         screenOptions={{
           headerShown: false,
         }}
       >
-        <Stack.Screen name="Dashboard" component={DashboardScreen} />
-        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Dashboard" component={LoggedInNav} />
         <Stack.Screen name="LogIn" component={LogInScreen} />
-        <Stack.Screen name="SignIn" component={SignInScreen} />
         <Stack.Screen name="Recovery" component={PassRecoveryScreen} />
-        <Stack.Screen name="Profile" component={ProfileScreen} />
         <Stack.Screen name="ChangePassword" component={ChangePasswordScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
-}
+};
