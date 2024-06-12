10d3305c559cf1dc675d9a6a3781a3f5fb45f28b
Ricardo Parra <ricardo.pxr16@gmail.com>
2024-06-12 18:05:49 -0400

    feat: gesturehandler for modal

--- App.js
+++ App.js
@@ -3,12 +3,14 @@ import { PaperProvider } from "react-native-paper";
 import { name as appName } from "./app.json";
 import { theme } from "./global/theme";
 import { LoginNav } from "./navigate/Navigator";
-
+import { GestureHandlerRootView } from "react-native-gesture-handler";
 export default function App() {
   return (
-    <PaperProvider theme={theme}>
-      <LoginNav />
-    </PaperProvider>
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <PaperProvider theme={theme}>
+        <LoginNav />
+      </PaperProvider>
+    </GestureHandlerRootView>
   );
 }
 
