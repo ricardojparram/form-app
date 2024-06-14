534f9ab0e371b9c6838eb3c12f192ff9a595c9f1
Ricardo Parra <ricardo.pxr16@gmail.com>
2024-06-14 18:18:08 -0400

    feat: nuevos colores primarios y secundarios

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./App.{js,jsx,ts,tsx}", "./screens/**/*.{js,jsx,ts,tsx}", "./components/*.{js,jsx,ts,tsx}"],
+  content: [
+    "./App.{js,jsx,ts,tsx}",
+    "./screens/**/*.{js,jsx,ts,tsx}",
+    "./components/*.{js,jsx,ts,tsx}",
+  ],
   theme: {
     extend: {
       colors: {
-        'theme': {
-          'primar' : "#386b01",
-          'second' : "#57624a",
-          'third' : "#386664",
-          'error' : '#ba1a1a',
-          'background': '#fdfdf5'
+        theme: {
+          primar: "#96bd0d",
+          second: "#7d9c0b",
+          third: "#386664",
+          error: "#ba1a1a",
+          background: "#fdfdf5",
         },
-      }
+      },
     },
   },
   extend: {},
   plugins: [],
-}
+};
