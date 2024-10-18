0c5846825ed8c4a853c32fee374b5bdc8ba2a4ac
Ricardo Parra <ricardo.prxr16@gmail.com>
2024-10-17 20:46:04 -0400

    feat: inyeccion mysql test done

--- tests/login.test.js
+++ tests/login.test.js
@@ -1,7 +1,7 @@
 import { act, renderHook } from "@testing-library/react-native";
 import { useAuthStore } from "../store/authStore";
 
-describe("useAuthStore", () => {
+describe("Login y authstore", () => {
   it("Debería setear y obtener usuario", async () => {
     const { result } = renderHook(() => useAuthStore());
 
@@ -10,6 +10,19 @@ describe("useAuthStore", () => {
     });
 
     expect(result.current.user).toEqual({ name: "Ricardo Parra" });
+  });
+
+  it("Debería manejar inyecciones SQL en el login", async () => {
+    const { result } = renderHook(() => useAuthStore());
+
+    const maliciousUsername = "' OR '1'='1";
+    const maliciousPassword = "' OR '1'='1";
+
+    await act(async () => {
+      await result.current.login(maliciousUsername, maliciousPassword);
+    });
+
+    expect(result.current.isAuthenticated).toBe(false);
   });
 
   it("Debería autenticar el usuario", async () => {
