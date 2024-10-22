32f9b3d6ea4e5c006de2830474a86aab8b642738
Ricardo Parra <ricardo.prxr16@gmail.com>
2024-10-21 22:40:39 -0400

    feat: donation test added

---
+++ tests/donation.test.js
@@ -0,0 +1,44 @@
+import { act, renderHook } from "@testing-library/react-native";
+import { useAuthStore } from "../store/authStore";
+import { API_SRC } from "../env";
+
+const session = {
+  token: "",
+};
+
+describe("Donaciones", () => {
+  it("Debería autenticar el usuario", async () => {
+    const { result } = renderHook(() => useAuthStore());
+
+    await act(async () => {
+      await result.current.login("1", "V-123123123", "123123123");
+    });
+    expect(result.current.isAuthenticated).toBe(true);
+    session.token = result.current.token;
+  });
+  it("Debería retornar las donaciones de la sede", async () => {
+    await act(async () => {
+      const res = await fetch(API_SRC + "?url=donativoPersonal&mostrar=&app=", {
+        headers: {
+          Authorization: `Bearer ${session.token}`,
+        },
+      });
+      const res_json = await res.json();
+      expect(Array.isArray(res_json)).toBe(true);
+    });
+  });
+  it("Debería retornar detalles de la donacion", async () => {
+    await act(async () => {
+      const id = "1";
+      const res = await fetch(
+        API_SRC + "?url=donativoPersonal&detalleD=&id=" + id,
+        {
+          headers: {
+            Authorization: `Bearer ${session.token}`,
+          },
+        }
+      ).then((res) => res.json());
+      expect(Array.isArray(res)).toBe(true);
+    });
+  });
+});
